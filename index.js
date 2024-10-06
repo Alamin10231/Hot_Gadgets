0787692f40346ea0d9974b54ad178b8d620e17c5
Md Alamin <alamin3636md@gmail.com>
2024-10-06 14:08:19 +0600

    secondk done

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
-const loadallphone =()=>{
+const loadallphone =async(status)=>{
     console.log("load all phone after 3 second");
      document.getElementById('spiner').style.display = "none";
-     fetch(`https://openapi.programming-hero.com/api/phones?search=iphone`)
-     .then((res)=>res.json())
-     .then((data) =>console.log(data))
+   const resposne =await fetch(`https://openapi.programming-hero.com/api/phones?search=iphone`);
+   const data = await resposne.json();
+     
+   if(status){
+    displayAllPhone(data.data)
+   }else{
+    displayAllPhone(data.data.slice(0,6))
+   }
 }
-
-
 const handle = () =>{
 
-     document.getElementById("spiner").style.display = "block";
-    setTimeout(function(){
-        loadallphone()
-    },2000)
+    document.getElementById("spiner").style.display = "block";
+   setTimeout(function(){
+       loadallphone()
+   },2000)
 }
+
+const displayAllPhone=(phones)=>{
+console.log(phones)
+}
+
+
+
+const handleShowAll=()=>{
+    loadallphone (true)
+}
+
+
